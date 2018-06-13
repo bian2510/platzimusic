55030d44ab1856a33892c4ceb819abc8a88c262e
bian2510 <bian251091@gmail.com>
2018-06-12 20:14:06 -0500

    platzimusic

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appName: "platzimusic",
+	apiKey: "36ff907f24808e62b6fe7285b5defa85",
+	secret: "c60eaa9c755964101706e9cd1b3ef6b0",
+	registeredTo: "bian2510"
+}
+
+//Application name	platzimusic
+//API key	36ff907f24808e62b6fe7285b5defa85
+//Shared secret	c60eaa9c755964101706e9cd1b3ef6b0
+//Registered to	bian2510
